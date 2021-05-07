1772cecfed9f43456e9458f4c2dc771e23407383
tjconti12 <tjconti12@gmail.com>
2021-05-07 12:41:17 -0400

    site under construction update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>This Site is Under Construction!</h1>
+      <h3>&copy; Tyler Conti 2021</h3>
+      <a href="https://github.com/tjconti12/" target="_blank">Link to github</a>
+      {" "}
+      <a href="https://www.linkedin.com/in/tylerjconti/" target="_blank">Link to linkedin</a>
     </div>
   );
 }
