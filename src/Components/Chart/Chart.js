31f7e362787cd07665971759ef0eb7e9a44bc02d
tjconti12 <tjconti12@gmail.com>
2021-05-18 10:01:37 -0400

    change chart to static image. comment out code for later use

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,49 +1,51 @@
-
-import { PieChart } from 'react-minimal-pie-chart';
+import chart from '../../Images/LanguageChart.png'
+// import { PieChart } from 'react-minimal-pie-chart';
 import * as chartStyles from './Chart.module.css';
 
-const Chart = ({ languages }) => {
+const Chart = () => {
 
   
 
 
-  let colors = ['yellow', 'green', 'pink', 'orange', 'red']
-  const labelStyle = {
-    fontSize: '4px'
-  }
+  // let colors = ['yellow', 'green', 'pink', 'orange', 'red']
+  // const labelStyle = {
+  //   fontSize: '4px'
+  // }
 
-  if(!languages) return <h1>Loading</h1>
+  // if(!languages) return <h1>Loading</h1>
 
   return (
-    
     <div className={chartStyles.chartContainer}>
-      <PieChart
-        data={languages.map((obj, index) => {
-            return {title: obj[0], value: obj[1], color: colors[index] ? colors[index] : colors[Math.floor(Math.random() * colors.length)]}
-        })}
-        label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
-        labelPosition={75}
-        labelStyle={{
-            ...labelStyle
-        }}
-        lineWidth={60}
-        paddingAngle={5}
-        animate={true}
-        animationDuration={500}
-        radius={20}
-        center={[60, 50]}
-    />
-    <div className={chartStyles.labelItemsContainer}>
-        {languages.map((obj, index) => {
-            return (
-                <div className={chartStyles.labelItem}>
-                    <h4 className={chartStyles.labelName}>{obj[0]}</h4>
-                    <div className={chartStyles.colorBox} style={{"backgroundColor" : colors[index]}}></div>
-                </div>
-            )
-        })}
+      <img className={chartStyles.chartImg} src={chart} alt="pie chart showing percentage of github languages"/>
     </div>
-    </div>
+    // <div className={chartStyles.chartContainer}>
+    //   <PieChart
+    //     data={languages.map((obj, index) => {
+    //         return {title: obj[0], value: obj[1], color: colors[index] ? colors[index] : colors[Math.floor(Math.random() * colors.length)]}
+    //     })}
+    //     label={({ dataEntry }) => `${Math.round(dataEntry.percentage)}%`}
+    //     labelPosition={75}
+    //     labelStyle={{
+    //         ...labelStyle
+    //     }}
+    //     lineWidth={60}
+    //     paddingAngle={5}
+    //     animate={true}
+    //     animationDuration={500}
+    //     radius={20}
+    //     center={[60, 50]}
+    // />
+    // <div className={chartStyles.labelItemsContainer}>
+    //     {languages.map((obj, index) => {
+    //         return (
+    //             <div className={chartStyles.labelItem}>
+    //                 <h4 className={chartStyles.labelName}>{obj[0]}</h4>
+    //                 <div className={chartStyles.colorBox} style={{"backgroundColor" : colors[index]}}></div>
+    //             </div>
+    //         )
+    //     })}
+    // </div>
+    // </div>
   )
 }
 
--- src/Components/Languages/Languages.js
+++ src/Components/Languages/Languages.js
@@ -1,61 +1,61 @@
-import { useState, useEffect } from 'react';
+// import { useState, useEffect } from 'react';
 import * as languageStyles from './Languages.module.css';
 import Logos from '../Logos/Logos';
 import Chart from '../Chart/Chart';
 
 const Languages = () => {
-    const [repos, setRepos] = useState(null);
-    const [languages, setLanguages] = useState(null);    
+    // const [repos, setRepos] = useState(null);
+    // const [languages, setLanguages] = useState(null);    
 
-    const getRepos = async () => {
-        try {
-            const response = await fetch('https://api.github.com/users/tjconti12/repos', {
-                headers: {
-                    Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-                }
-            });
-            const data = await response.json();
-            setRepos(data)
-        } catch (error) {
-            console.log(error)
-        }
-    }
+    // const getRepos = async () => {
+    //     try {
+    //         const response = await fetch('https://api.github.com/users/tjconti12/repos', {
+    //             headers: {
+    //                 Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+    //             }
+    //         });
+    //         const data = await response.json();
+    //         setRepos(data)
+    //     } catch (error) {
+    //         console.log(error)
+    //     }
+    // }
 
 
-    const getLanguages = () => {
-        let languageTotal = [];
-        if(repos) {
-            repos.map(async repo => {
-                try {
-                    const response = await fetch(repo.languages_url, {
-                        headers: {
-                            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-                        }
-                    });
-                    const data = await response.json();
-                    for(const key in data){
-                        if(languageTotal[key]){
-                            languageTotal[key] += data[key]
-                        } else {
-                            languageTotal[key] = data[key]
-                        }
-                    }
-                    setLanguages(Object.entries(languageTotal))
-                } catch (error) {
-                    console.log(error)
-                }
+    // const getLanguages = () => {
+    //     let languageTotal = [];
+    //     if(repos) {
+    //         repos.map(async repo => {
+    //             try {
+    //                 const response = await fetch(repo.languages_url, {
+    //                     headers: {
+    //                         Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
+    //                     }
+    //                 });
+    //                 const data = await response.json();
+    //                 for(const key in data){
+    //                     if(languageTotal[key]){
+    //                         languageTotal[key] += data[key]
+    //                     } else {
+    //                         languageTotal[key] = data[key]
+    //                     }
+    //                 }
+    //                 setLanguages(Object.entries(languageTotal))
+    //             } catch (error) {
+    //                 console.log(error)
+    //             }
                 
-            })
-        }
-    }
+    //         })
+    //     }
+    // }
 
-    useEffect(() => {
-        getRepos();
-    }, [])
+    // useEffect(() => {
+    //     getRepos();
+    // }, [])
 
-    useEffect(() => {
-        if(repos) getLanguages()
-    }, [repos])
+    // useEffect(() => {
+    //     if(repos) getLanguages()
+    // }, [repos])
 
     
 
@@ -63,10 +63,10 @@ const Languages = () => {
         <div className={languageStyles.languageSection}>
             <div className={languageStyles.container}>
                 <div className={languageStyles.leftColumn}>
-                {languages ?
-                    <Chart languages={languages} />
-                    : <h2 className={languageStyles.failedToLoad}>Sorry! The Data Failed To Load From Github. View the stats at <a target="_blank" rel="noopener noreferrer" href="https://github.com/tjconti12/">Github</a></h2>
-                }
+                {/* {languages ? */}
+                    <Chart />
+                    {/* : <h2 className={languageStyles.failedToLoad}>Sorry! The Data Failed To Load From Github. View the stats at <a target="_blank" rel="noopener noreferrer" href="https://github.com/tjconti12/">Github</a></h2> */}
+                {/* } */}
                 </div>
                 <div className={languageStyles.rightColumn}>
                     <Logos />
